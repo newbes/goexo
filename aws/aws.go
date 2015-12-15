cad3c8ebd6f7d157933c66256ec6159eaca59baf
sebastien.bui <sebastien.bui@maylab.my>
2015-12-15 12:32:02 +0800

    add exoscale

--- aws/aws.go
+++ aws/aws.go
@@ -85,6 +85,7 @@ var Regions = map[string]Region{
 	USGovWest.Name:    USGovWest,
 	SAEast.Name:       SAEast,
 	CNNorth1.Name:     CNNorth1,
+	Exoscale.Name:     Exoscale,
 }
 
 // Designates a signer interface suitable for signing AWS requests, params
